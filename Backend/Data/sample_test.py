cd197522479e3e52c2d7c4c625bc7d7468b15303
ayushkumar9576 <ayushmth9576@gmail.com>
2026-03-27 14:44:00 +0530

    Wrote Code For  Storage

--- Backend/Data/sample_test.py
+++ Backend/Data/sample_test.py
@@ -32,16 +32,16 @@ def generate_Sample(n: int = 500, seed: int = 42)->pd.DataFrame:
     burst_time = np.clip((0.6*np.where(new_processor,default_new,prev_burst_avg)+type_bias+rng.normal(0,1.5,size=n)),0.5,25.0)
 
     dataFrame = pd.DataFrame({
-        "Arrival time":np.round(arrival_time,5),
-        "Previous Burst Count":prev_burst_count.astype(int),
-        "Previous Burst Average": np.round(prev_burst_avg,5),
-        "Process Type":type_Process.astype(int),
-        "Burst Time":np.round(burst_time,5)
+        "arrival_time":np.round(arrival_time,5),
+        "prev_burst_count":prev_burst_count.astype(int),
+        "prev_burst_avg": np.round(prev_burst_avg,5),
+        "process_type":type_Process.astype(int),
+        "burst_time":np.round(burst_time,5)
     })
     return dataFrame
 
 def getFeature(df: pd.DataFrame)->np.ndarray:
-    return df[["Arrival time","Previous Burst Count","Previous Burst Average","Process Type"]].to_numpy()
+    return df[["arrival_time","prev_burst_count","prev_burst_avg","process_type"]].to_numpy()
 
 def getBurst(df:pd.DataFrame)->np.ndarray:
-    return df["Burst Time"].to_numpy()
+    return df["burst_time"].to_numpy()

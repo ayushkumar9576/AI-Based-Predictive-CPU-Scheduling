a5340d09f448d232b9a0a2e7070a3847c4e2b696
ayushkumar9576 <ayushmth9576@gmail.com>
2026-03-29 15:20:05 +0530

    Added Round Robin Algo

---
+++ Backend/Algorithms/round_robin.py
@@ -0,0 +1,8 @@
+import copy
+from coreLogic.calculation import calculate_parameter, average_turnaround_time, average_waiting_time
+
+def gantt_chart(timeline):
+    None
+
+def round_robin(processes):
+    None
